49fd713771d194590e2d9861538abc78cb3d9f30
snowlandsontop <152302239+snowlandsontop@users.noreply.github.com>
2024-01-16 10:24:16 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,51 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    music.setVolume(60)
-    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
     for (let index = 0; index < 3; index++) {
+        pins.servoWritePin(AnalogPin.P0, 180)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            `)
         pins.servoWritePin(AnalogPin.P0, 0)
         basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # # .
+            `)
         pins.servoWritePin(AnalogPin.P0, 180)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . # # # .
+            # # # # #
+            `)
+        pins.servoWritePin(AnalogPin.P0, 0)
+        basic.pause(200)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            `)
+        pins.servoWritePin(AnalogPin.P0, 180)
+        basic.pause(200)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        pins.servoWritePin(AnalogPin.P0, 0)
         basic.pause(200)
     }
 })
@@ -15,53 +56,15 @@ input.onSound(DetectedSound.Loud, function () {
     radio.sendString("back")
 })
 radio.onReceivedString(function (receivedString) {
+    music.setVolume(60)
+    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
     for (let index = 0; index < 3; index++) {
-        pins.servoWritePin(AnalogPin.P0, 180)
-        basic.pause(200)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . # . .
-            `)
         pins.servoWritePin(AnalogPin.P0, 0)
         basic.pause(200)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . # . .
-            . # # # .
-            `)
         pins.servoWritePin(AnalogPin.P0, 180)
-        basic.pause(200)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . # # # .
-            # # # # #
-            `)
-        pins.servoWritePin(AnalogPin.P0, 0)
-        basic.pause(200)
-        basic.showLeds(`
-            . . . . .
-            . . # . .
-            . # # # .
-            # # # # #
-            . . # . .
-            `)
-        pins.servoWritePin(AnalogPin.P0, 180)
-        basic.pause(200)
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # # # # #
-            . . # . .
-            . . # . .
-            `)
-        pins.servoWritePin(AnalogPin.P0, 0)
         basic.pause(200)
     }
+})
+input.onGesture(Gesture.Shake, function () {
+	
 })

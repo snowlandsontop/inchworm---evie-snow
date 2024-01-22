aa90af0f4e9cdf070d84a94aba2c8be2bc7806f3
snowlandsontop <152302239+snowlandsontop@users.noreply.github.com>
2024-01-22 09:59:12 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -60,9 +60,7 @@ input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
 })
 radio.onReceivedString(function (receivedString) {
-    music.setVolume(60)
     for (let index = 0; index < 3; index++) {
-        music.play(music.stringPlayable("A F - C - - - - ", 120), music.PlaybackMode.UntilDone)
         pins.servoWritePin(AnalogPin.P0, 0)
         basic.pause(200)
         pins.servoWritePin(AnalogPin.P0, 180)
@@ -71,4 +69,6 @@ radio.onReceivedString(function (receivedString) {
 })
 input.onGesture(Gesture.Shake, function () {
     radio.sendString("back")
+    music.setVolume(60)
+    music.play(music.stringPlayable("A G F E E F G A ", 120), music.PlaybackMode.UntilDone)
 })
